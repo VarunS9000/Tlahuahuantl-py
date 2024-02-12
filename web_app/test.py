adda03c0325ef48149e34c0d53cb5868237bf1a6
VarunS9000 <varunsreedhar14@gmail.com>
2024-02-12 12:55:03 -0500

    Fixed encoding issue. Introduced highlighting.

--- web_app/test.py
+++ web_app/test.py
@@ -15,12 +15,16 @@ with open(f'norm_ref_map.pkl', 'rb') as pickle_file:
 
 for k in range(3,29):
     to_remove = []
+
     for ele in ref_map[k]:
+        
         if len(ele.strip()) == 0:
             to_remove.append(ele)
-    
+            
     for t in to_remove:
         ref_map[k].remove(t)
+    
+    
         
 
 characters_list = ["(" + str(i) + ")" for i in range(1, 51)]
@@ -29,6 +33,15 @@ for k in range(3,29):
     for i in range(len(ref_map[k])):
         for c in characters_list:
             ref_map[k][i] = ref_map[k][i].replace(c,'')
+            
+
+for k in range(3,29):
+    for cnt in range(len(ref_map[k])):
+        ref_map[k][cnt] = ref_map[k][cnt].replace('Ã§','ç')
+        ref_map[k][cnt] = ref_map[k][cnt].replace('Â§','Ç')
+        
+        norm_ref_map[k][cnt] = norm_ref_map[k][cnt].replace('Ã§','ç')
+        norm_ref_map[k][cnt] = norm_ref_map[k][cnt].replace('Â§','Ç')
 
 
 for i in range(3,29):
